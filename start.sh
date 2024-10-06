4ace6c362e0633a25976c2b03eac8eebeaf3bcef
Bauyrzhan Tokenov <bauyrzhan.a.tokenov@gmail.com>
2024-10-06 15:07:28 +0200

    Fix shell script

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 source backend/venv/bin/activate
 
 # start backend
-(cd backend && pip install -r requirements.txt && python app.py) & 
+(cd backend && pip install -r requirements.txt && python3 app.py) & 
 
 # start frontend
 (cd frontend && npm install && npm run dev)
